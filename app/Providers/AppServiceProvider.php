029e020a668bf33588c1defcb833d39432d7439c
Irfan <qcutterz@gmail.com>
2021-12-05 06:50:48 +0700

    news categories database, news database, visitor database

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,11 @@
 
 namespace App\Providers;
 
+use App\Helpers\GlobalHelper;
+use App\Models\Visitor;
 use Illuminate\Support\ServiceProvider;
+use Jenssegers\Agent\Agent;
+use Stevebauman\Location\Facades\Location;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +27,62 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        /* VISITOR */
+        $is_visitor = GlobalHelper::isVisitor();
+
+        if(!$is_visitor)
+        {
+            $agent = new Agent();
+            $ip_address = request()->ip();
+            $location = Location::get($ip_address);
+            $device = 'Desktop';
+
+            if($agent->isMobile())
+            {
+                $device = 'Mobile';
+            }
+            
+            elseif($agent->isTablet())
+            {
+                $device = 'Tablet';
+            }
+            
+            elseif($agent->isPhone())
+            {
+                $device = 'Phone';
+            }
+            
+            elseif($agent->isRobot())
+            {
+                $device = 'Robot';
+            }
+
+            $visitor = [
+                'ip_address' => $ip_address,
+                'country_name' => $location ? $location->countryName : null,
+                'country_code' => $location ? $location->countryCode : null,
+                'region_code' => $location ? $location->regionCode : null,
+                'region_name' => $location ? $location->regionName : null,
+                'city_name' => $location ? $location->cityName : null,
+                'zip_code' => $location ? $location->zipCode : null,
+                'iso_code' => $location ? $location->isoCode : null,
+                'postal_code' => $location ? $location->postalCode : null,
+                'latitude' => $location ? $location->latitude : null,
+                'longitude' => $location ? $location->longitude : null,
+                'metro_code' => $location ? $location->metroCode : null,
+                'area_code' => $location ? $location->areaCode : null,
+                'device' => $device,
+                'device_name' => $agent->device(),
+                'platform' => $agent->platform(),
+                'platform_version' => $agent->version($agent->platform()),
+                'browser' => $agent->browser(),
+                'browser_version' => $agent->version($agent->browser()),
+                'last_visited_at' => \Carbon\Carbon::now()
+            ];
+
+            Visitor::create($visitor);
+        } else {
+            $is_visitor->update(['last_visited_at' => \Carbon\Carbon::now()]);
+        }
     }
 }
