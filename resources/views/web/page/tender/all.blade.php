b932b5f7d6a1e1a872bc021c18fafa62b022aee7
wahyu <jekomontainugrah@gmail.com>
2021-12-14 04:53:09 -0800

    tender dll

---
+++ resources/views/web/page/tender/all.blade.php
@@ -0,0 +1,57 @@
+<section class="blog-listing ptb80" id="version1">
+    <div class="container">
+        <div class="row">
+
+            <!-- Start of Blog Posts -->
+            <div class="col-md-12 col-xs-12 blog-posts-wrapper">
+
+                <!-- Start of Blog Post Article 1 -->
+                <article class="col-md-12 blog-post">
+
+                    <!-- Blog Post Thumbnail -->
+                    <div class="col-md-4 blog-thumbnail">
+                        <a href="blog-post-right-sidebar.html" class="hover-link"><img src="" class="img-responsive" alt=""></a>
+                        <div class="date">
+                            <span class="day">11</span>
+                            <span class="publish-month">Mar</span>
+                        </div>
+                    </div>
+
+                    <!-- Blog Post Description -->
+                    <div class="col-md-8 blog-desc">
+                        <h5><a href="blog-post-right-sidebar.html">PT.Picotel Nusantara</a></h5>
+                        <!-- <div class="post-detail pt10 pb20">
+                            <span><i class="fa fa-user"></i>Author</span>
+                        </div> -->
+                        <table class="mt5 mb5">
+                            <tr>
+                                <th style=" padding-left: 5px; padding-right: 5px; background-color: rgb(192, 148, 52); color: rgb(255, 255, 255); border-radius: 5px; margin-inline-end: 10px;"> StartDate: 12/05/2021</th>
+                                <th style="padding-right : 10px; padding-left: 10px;">=></th>
+                                <th style="padding-left: 5px; padding-right: 5px;background-color: rgb(158, 90, 51); color: rgb(255, 255, 255); border-radius: 5px;">EndDate: 25/05/2021</th>
+                            </tr>
+                        </table>
+
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque reprehenderit itaque, quibusdam dolorem corrupti possimus delectus alias necessitatibus magni ea laborum minima natus mollitia laboriosam fugit! Pariatur quibusdam eos dicta consequuntur labore quos repellat tempora possimus cupiditate, ipsum et illum ratione! Placeat molestiae cum sequi, vel nobis aperiam, non ex ratione consequatur tempore ducimus officia dignissimos enim atque odio ipsam?</p>
+                        <a href="" class="btn btn-blue btn-effect mt10">read more</a>
+                        
+                    </div>
+                </article>
+                
+                <!-- End of Blog Post Article 1 -->
+
+                <!-- Start of Pagination -->
+                <div class="col-md-12">
+                    <ul class="pagination list-inline text-center">
+                        Link
+                    </ul>
+                </div>
+                <!-- End of Pagination -->
+
+            </div>
+            <!-- End of Blog Posts -->
+
+
+         
+        </div>
+    </div>
+</section>
