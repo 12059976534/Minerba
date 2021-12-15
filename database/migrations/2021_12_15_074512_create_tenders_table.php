d5dbaada1ab08e97413f8b7e3a2098ce34559bfb
Irfan <qcutterz@gmail.com>
2021-12-15 14:50:27 +0700

    tender migration

---
+++ database/migrations/2021_12_15_074512_create_tenders_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTendersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tenders', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('company_id');
+            $table->date('start_data');
+            $table->date('end_date');
+            $table->longText('description');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tenders');
+    }
+}
